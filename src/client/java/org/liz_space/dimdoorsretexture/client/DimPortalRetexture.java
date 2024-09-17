15d925c5ea2ed20d5853f6d7802b602ecf96d4ba
Liz C. <alicecfire@gmail.com>
2024-09-17 17:28:33 -0300

    refactor(build): restructure Gradle build script and project files
    - Moved resource pack files to src/client/resources/resourcepacks/default directory.
    - Renamed Main.java to DimDoorsRetextureMain.java.
    - Updated Gradle build script for better dependency management.
    - Added LazyDFU and Mod Menu as dev dependencies.
    - Updated fabric.mod.json to reflect new file paths and author information.
    - Enhanced DimPortalRetexture client initialization with built-in resource packs.
    
    This commit improves project organization and builds configuration.
    
    Signed-off-by: Liz C. <alicecfire@gmail.com>

--- src/client/java/org/liz_space/dimdoorsretexture/client/DimPortalRetexture.java
+++ src/client/java/org/liz_space/dimdoorsretexture/client/DimPortalRetexture.java
@@ -2,14 +2,28 @@ package org.liz_space.dimdoorsretexture.client;
 
 import com.mojang.logging.LogUtils;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 
 public class DimPortalRetexture implements ClientModInitializer {
     public static final Logger LOGGER = LogUtils.getLogger();
+    public static final String MOD_ID = "dimdoorsretexture";
 
     @Override
     public void onInitializeClient() {
-        LOGGER.atInfo().log("tampering with dimPortal models");
+        LOGGER.info("tampering with dimPortal rendering");
 
+//        public static final ResourceLocation id = new ResourceLocation("dimdoors");
+
+        FabricLoader.getInstance().getModContainer(MOD_ID).ifPresent(modContainer -> {
+            ResourceManagerHelper.registerBuiltinResourcePack(
+                    new ResourceLocation(MOD_ID, "default"),
+                    modContainer,
+                    ResourcePackActivationType.DEFAULT_ENABLED
+            );
+        });
     }
 }
--- src/main/java/org/liz_space/dimdoorsretexture/DimDoorsRetextureMain.java
+++ src/main/java/org/liz_space/dimdoorsretexture/DimDoorsRetextureMain.java
@@ -2,7 +2,7 @@ package org.liz_space.dimdoorsretexture;
 
 import net.fabricmc.api.ModInitializer;
 
-public class Main implements ModInitializer {
+public class DimDoorsRetextureMain implements ModInitializer {
 
     @Override
     public void onInitialize() {
