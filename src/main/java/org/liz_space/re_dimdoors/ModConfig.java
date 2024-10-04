d19fa7ad25a2cd0ffec214cb74fe1ad94ee848b4
Liz C. <alicecfire@gmail.com>
2024-10-04 16:12:58 -0300

    feat(mixin): add random escape target world functionality
    This commit introduces a feature that allows the selection of a random escape target world when a player attempts to escape from limbo. Key changes include:
    
    - Enhanced the `EscapeTargetMixin` class to include a method for retrieving a random world from a configured list of escape targets. If a selected world is invalid, it is removed from the list.
    - Modified the logic for determining the escape target world to utilize the new randomization feature.
    - Updated the `ModConfig` class to accommodate a list of possible escape dimensions, as well as a default escape target world configuration.
    - Added relevant entries to the localization file for the new configuration options.
    
    Additionally, changes were made to ensure that the random escape target world feature integrates smoothly with existing limbo escape logic, enhancing the overall gameplay experience.
    
    Signed-off-by: Liz C. <alicecfire@gmail.com>

--- src/main/java/org/liz_space/re_dimdoors/ModConfig.java
+++ src/main/java/org/liz_space/re_dimdoors/ModConfig.java
@@ -5,7 +5,11 @@ import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
 import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 
-import static org.liz_space.re_dimdoors.RE_DimDoorsMain.MOD_ID;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.liz_space.re_dimdoors.RE_DimDoorsMain.*;
 
 @Config(name = MOD_ID)
 @Config.Gui.Background(value = "dimdoors:textures/other/warp.png")
@@ -20,34 +24,80 @@ public class ModConfig implements ConfigData {
     @ConfigEntry.Category("Client")
     @ConfigEntry.Gui.PrefixText
     public Client client = new Client();
+    @Override
+    public void validatePostLoad() throws ValidationException {
+        this.common.worldList = this.common.worldList.stream().distinct().collect(Collectors.toList());
+        if (this.common.worldList.isEmpty()) {this.common.worldList.add(this.common.defaultEscapeTargetWorld);}
+
+        ConfigData.super.validatePostLoad();
+    }
+
     public static class Common {
 
 
-        @Comment("patches the Detached Rift decay function, making the decay pattern spherical instead of cubic.")
+        @Comment("""
+                 List of possible limbo escape dimensions:
+                 <namespace>:<dimension_id>
+                 """)
+        public List<String> worldList = new ArrayList<>();
+        @Comment("""
+                 Fallback value for EscapeTarget dimension.
+                 if invalid value is set, things may break.
+                 """)
+        public String defaultEscapeTargetWorld = "minecraft:overworld";
+        @Comment("""
+                 patches the Detached Rift decay function,
+                 making the decay pattern spherical instead of cubic.
+                 """)
         boolean radialDecayMixin = true;
+        @ConfigEntry.Gui.Excluded
+        @Comment("""
+                 Uses the default limbo escape world
+                 in lieu of Implementing a list of possible escape targets.
+                 
+                 Current Implementation of EscapeTargetMixin may break if true
+                 """)
+        boolean useEscapeTargetWorld = false;
 
         @Comment("Activates debug logging for the mod")
         @ConfigEntry.Gui.Excluded
         boolean debug = true;
 
-        public boolean getDebug() {
-            return debug;
+        public boolean getDebug() {return this.debug;}
+
+        public boolean getRadialDecayMixin() {return this.radialDecayMixin;}
+
+        public boolean getUseEscapeTargetWorld() {return this.useEscapeTargetWorld;}
+
+        public String getDefaultEscapeTargetWorld() {return this.defaultEscapeTargetWorld;}
+
+        public List<String> getWorldList() {return this.worldList;}
+
+        public void rmFromWorldList(String world) {
+            MAIN_LOGGER.info("Removing faulty world {} from EscapeTarget worldList...", world);
+            this.worldList.remove(world);
+            if (this.worldList.isEmpty()) this.worldList.add(CONFIG.common.getDefaultEscapeTargetWorld());
         }
 
-        public boolean getRadialDecayMixin() {
-            return radialDecayMixin;
-        }
     }
 
     public static class Client {
 
         @ConfigEntry.Gui.CollapsibleObject
         @ConfigEntry.Gui.TransitiveObject
-        @Comment("Edit values for the portal polygons.\nRequires restart.")
+        @Comment("""
+                 Edit values for the portal polygons.
+                 Requires restart.
+                 """)
         public DimPortalRendererMixin dimPortalRenderer = new DimPortalRendererMixin();
         @ConfigEntry.Gui.RequiresRestart
-        @Comment("Enables editing values for the portal polygons.\nRequires restart")
+        @Comment("""
+                 Enables editing values for the portal polygons.
+                 Requires restart
+                 """)
         boolean dimPortalRendererMixin = false;
+
+        public boolean getDimPortalRendererMixin() {return this.dimPortalRendererMixin;}
 
         public static class DimPortalRendererMixin {
 
@@ -71,29 +121,17 @@ public class ModConfig implements ConfigData {
                 @ConfigEntry.Gui.RequiresRestart
                 float dimensionZ = .01F;
 
-                public float getDimensionX() {
-                    return dimensionX;
-                }
+                public float getDimensionX() {return this.dimensionX;}
 
-                public float getDimensionY() {
-                    return dimensionY;
-                }
+                public float getDimensionY() {return this.dimensionY;}
 
-                public float getDimensionZ() {
-                    return dimensionZ;
-                }
+                public float getDimensionZ() {return this.dimensionZ;}
 
-                public float getOriginX() {
-                    return originX;
-                }
+                public float getOriginX() {return this.originX;}
 
-                public float getOriginY() {
-                    return originY;
-                }
+                public float getOriginY() {return this.originY;}
 
-                public float getOriginZ() {
-                    return originZ;
-                }
+                public float getOriginZ() {return this.originZ;}
             }
 
             public static class smallPortal {
@@ -110,36 +148,18 @@ public class ModConfig implements ConfigData {
                 @ConfigEntry.Gui.RequiresRestart
                 float dimensionZ = .01F;
 
-                public float getDimensionX() {
-                    return dimensionX;
-                }
+                public float getDimensionX() {return this.dimensionX;}
 
-                public float getDimensionY() {
-                    return dimensionY;
-                }
+                public float getDimensionY() {return this.dimensionY;}
 
-                public float getDimensionZ() {
-                    return dimensionZ;
-                }
+                public float getDimensionZ() {return this.dimensionZ;}
 
-                public float getOriginX() {
-                    return originX;
-                }
+                public float getOriginX() {return this.originX;}
 
-                public float getOriginY() {
-                    return originY;
-                }
+                public float getOriginY() {return this.originY;}
 
-                public float getOriginZ() {
-                    return originZ;
-                }
+                public float getOriginZ() {return this.originZ;}
             }
         }
-
-        public boolean getDimPortalRendererMixin() {
-            return dimPortalRendererMixin;
-        }
     }
-
-
 }
--- src/main/java/org/liz_space/re_dimdoors/mixin/EscapeTargetMixin.java
+++ src/main/java/org/liz_space/re_dimdoors/mixin/EscapeTargetMixin.java
@@ -4,6 +4,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Rotations;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
@@ -22,28 +23,44 @@ import org.dimdev.dimdoors.rift.targets.EscapeTarget;
 import org.dimdev.dimdoors.world.ModDimensions;
 import org.spongepowered.asm.mixin.*;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
+
+import static net.minecraft.core.registries.Registries.DIMENSION;
+import static org.dimdev.dimdoors.DimensionalDoors.getWorld;
+import static org.liz_space.re_dimdoors.RE_DimDoorsMain.CONFIG;
 
 @Debug(export = true)
 @Mixin(EscapeTarget.class)
 public class EscapeTargetMixin {
 
     @Final
-    @Shadow
+    @Shadow(remap = false)
     private static Logger LOGGER;
 
-    @Shadow
-    private static ResourceKey<Level> targetWorldResourceKey;
-
     @Final
-    @Shadow
+    @Shadow(remap = false)
     protected boolean canEscapeLimbo;
 
 
-    @Shadow
+    @Shadow(remap = false)
     public static Location randomizeLimboReturn(Location loc, int range) {
         return null;
+    }
+
+
+    @Unique
+    protected ResourceKey<Level> getTargetWorldResourceKey(RandomSource random) {
+        List<String> list = CONFIG.common.getWorldList();
+        int index = random.nextInt(list.size());
+        String world = list.get(index);
+        ResourceKey<Level> key = ResourceKey.create(DIMENSION, new ResourceLocation(world));
+        if (getWorld(key) == null) {
+            CONFIG.common.rmFromWorldList(world);
+            return getTargetWorldResourceKey(random);
+        }
+        return key;
     }
 
     /**
@@ -55,6 +72,11 @@ public class EscapeTargetMixin {
         UUID uuid = entity.getUUID(); // honestly, things deserve to blow up if somehow there's no UUID
         ServerPlayer player = (ServerPlayer) Objects.requireNonNull(entity.level().getPlayerByUUID(uuid));
         Location destLoc;
+        RandomSource random = RandomSource.create();
+
+        ResourceKey<Level> escapeTargetWorld = (!CONFIG.common.getUseEscapeTargetWorld())
+                                               ? DimensionalDoors.getConfig().getLimboConfig().escapeTargetWorld
+                                               : getTargetWorldResourceKey(random);
 
         class SendLocation {
             static Location playerSpawn(ServerPlayer player) {
@@ -67,43 +89,40 @@ public class EscapeTargetMixin {
                 return new Location(DimensionalDoors.getServer().overworld(), DimensionalDoors.getServer().overworld().getSharedSpawnPos());
             }
 
-            static Location escapeTargetWorld(Entity entity) {
+            static Location escapeTargetWorld(Entity entity, ResourceKey<Level> world) {
                 LOGGER.log(org.apache.logging.log4j.Level.INFO, "Sending player from limbo to the exit dimension, good luck!");
-                return new Location(DimensionalDoors.getWorld(targetWorldResourceKey), new BlockPos(entity.blockPosition().getX(), DimensionalDoors.getConfig().getLimboConfig().escapeTargetWorldYSpawn, entity.blockPosition().getZ()));
+                return new Location(getWorld(world),
+                                    new BlockPos(entity.blockPosition().getX(),
+                                                 DimensionalDoors.getConfig().getLimboConfig().escapeTargetWorldYSpawn,
+                                                 entity.blockPosition().getZ()
+                                    ));
             }
         }
 
 
         if (// returns false if any of the following statements are true.
-                (!ModDimensions.isPocketDimension(entity.level()) && !ModDimensions.isLimboDimension(entity.level())) ||
-                        (ModDimensions.isLimboDimension(entity.level()) && !this.canEscapeLimbo) ||
-                        (entity.level().isClientSide)
-        ) {
-            return false;
-        } else if (entity.level().getPlayerByUUID(uuid) == null) {
+                (!ModDimensions.isPocketDimension(entity.level()) && !ModDimensions.isLimboDimension(entity.level()))
+                || (ModDimensions.isLimboDimension(entity.level()) && !this.canEscapeLimbo)
+                || (entity.level().isClientSide)
+        ) {return false;}
+
+        else if (entity.level().getPlayerByUUID(uuid) == null) {
             LOGGER.log(org.apache.logging.log4j.Level.ERROR, "Tried to get player for escape target from uuid, but player does not exist, uh oh");
             return false;
-        } else if (
-                player.getRespawnPosition() != null &&
-                        DimensionalDoors.getConfig().getLimboConfig().escapeTargetWorld == null &&
-                        !DimensionalDoors.getConfig().getLimboConfig().escapeToWorldSpawn
-        ) {
-            destLoc = SendLocation.playerSpawn(player);
-        } else if (
-                DimensionalDoors.getConfig().getLimboConfig().escapeTargetWorld != null &&
-                        !DimensionalDoors.getConfig().getLimboConfig().escapeToWorldSpawn
-        ) {
-            targetWorldResourceKey = DimensionalDoors.getConfig().getLimboConfig().escapeTargetWorld;
-
-            if (DimensionalDoors.getWorld(targetWorldResourceKey) != null) {
-                destLoc = SendLocation.escapeTargetWorld(entity);
-            } else {
-                LOGGER.log(org.apache.logging.log4j.Level.INFO, "Target dimension defined in config does not exist.  Use /forge dimensions for a list!");
-                destLoc = SendLocation.worldSpawn();
-            }
-        } else {
-            destLoc = SendLocation.worldSpawn();
         }
+
+        else if (
+                player.getRespawnPosition() != null
+                && escapeTargetWorld == null
+                && !DimensionalDoors.getConfig().getLimboConfig().escapeToWorldSpawn
+        ) {destLoc = SendLocation.playerSpawn(player);}
+
+        else if (
+                escapeTargetWorld != null
+                && !DimensionalDoors.getConfig().getLimboConfig().escapeToWorldSpawn
+        ) {destLoc = SendLocation.escapeTargetWorld(entity, escapeTargetWorld);}
+
+        else {destLoc = SendLocation.worldSpawn();}
 
         destLoc = randomizeLimboReturn(destLoc, DimensionalDoors.getConfig().getLimboConfig().limboReturnDistance);
         if (destLoc != null && this.canEscapeLimbo) {
@@ -112,20 +131,27 @@ public class EscapeTargetMixin {
             entity.fallDistance = -500.0F;
             location.getWorld().setBlockAndUpdate(location.getBlockPos(), Blocks.AIR.defaultBlockState());
             location.getWorld().setBlockAndUpdate(location.getBlockPos().offset(0, 1, 0), Blocks.AIR.defaultBlockState());
-            RandomSource random = RandomSource.create();
             BlockPos.withinManhattan(location.pos.offset(0, -3, 0), 3, 2, 3).forEach((pos1) -> {
                 if (random.nextFloat() < 1.0F / (float) location.pos.distSqr(pos1) * DimensionalDoors.getConfig().getLimboConfig().limboBlocksCorruptingExitWorldAmount) {
                     Block block = location.getWorld().getBlockState(pos1).getBlock();
                     if (UnravelUtil.unravelBlocksMap.containsKey(block)) {
-                        location.getWorld().setBlockAndUpdate(pos1, ((Block) UnravelUtil.unravelBlocksMap.get(block)).defaultBlockState());
-                    } else if (UnravelUtil.whitelistedBlocksForLimboRemoval.contains(block)) {
-                        location.getWorld().setBlockAndUpdate(pos1, ((Block) ModBlocks.UNRAVELLED_FABRIC.get()).defaultBlockState());
+                        location.getWorld().setBlockAndUpdate(pos1,
+                                                              UnravelUtil.unravelBlocksMap.get(block).defaultBlockState());
+                    }
+
+                    else if (UnravelUtil.whitelistedBlocksForLimboRemoval.contains(block)) {
+                        location.getWorld().setBlockAndUpdate(pos1,
+                                                              ModBlocks.UNRAVELLED_FABRIC.get().defaultBlockState());
                     }
                 }
             });
-        } else if (destLoc == null) {
+        }
+
+        else if (destLoc == null) {
             EntityUtils.chat(entity, Component.translatable("rifts.destinations.escape.did_not_use_rift"));
-        } else {
+        }
+
+        else {
             EntityUtils.chat(entity, Component.translatable("rifts.destinations.escape.rift_has_closed"));
         }
 
